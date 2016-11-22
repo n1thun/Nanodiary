389c2c4cf0f2345832b3cc7b4c9828b7c78d212d
Nithun Harikrishnan <nithun@loop-depaulsecure-231-50.depaulsecure-student.depaul.edu>
2016-11-21 19:33:07 -0600

    Login screen func added

---
+++ app/src/main/java/com/nithun/app/firediary/DisplayActivity.java
@@ -0,0 +1,13 @@
+package com.nithun.app.firediary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+    }
+}
--- app/src/main/java/com/nithun/app/firediary/MainActivity.java
+++ app/src/main/java/com/nithun/app/firediary/MainActivity.java
@@ -1,27 +1,91 @@
 package com.nithun.app.firediary;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import com.firebase.client.Firebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText mEmailField;
     private EditText mPasswordField;
+    private Button mLoginBtn;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
 
-
-
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        mEmailField = (EditText) findViewById(R.id.emailField);
+        mPasswordField = (EditText) findViewById(R.id.passwordField);
+
+        mLoginBtn = (Button) findViewById(R.id.loginBtn);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth){
+                if(firebaseAuth.getCurrentUser() != null ){
+                    startActivity(new Intent(MainActivity.this, DisplayActivity.class));
+                }
+            }
+        };
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startSignIn();
+            }
+        });
+
+
+
+    }
+
+    @Override
+    protected void onStart(){ //Listens for changes to Auth
+        super.onStart();
+
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    private void startSignIn(){
+
+        String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
+
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
+
+            Toast.makeText(MainActivity.this, "Fields are empty", Toast.LENGTH_LONG).show();
+        }
+        else {
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (!task.isSuccessful()){
+
+                        Toast.makeText(MainActivity.this, "Wrong username or password", Toast.LENGTH_LONG).show();
+
+
+                    }
+                }
+            });
+
+        }
 
     }
 }
